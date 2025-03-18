183a8267457f45cd19e4fd80385b0aba74be04d4
Sunitha Aragonda <sunitha_aragonda@epam.com>
2025-03-18 15:48:17 +0530

    first commit

---
+++ src/test/java/com/epam/onboardingproject/corejava/Task02_HometaskBasics.java
@@ -0,0 +1,10 @@
+package com.epam.onboardingproject.corejava;
+
+public class Task02_HometaskBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/epam/onboardingproject/corejava/task01_HometaskVariable.java
@@ -0,0 +1,10 @@
+package com.epam.onboardingproject.corejava;
+
+public class task01_HometaskVariable {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}

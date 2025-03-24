bbaaac55e8d8f2c9395fd518b3f03729b6757cd1
Sunitha Aragonda <sunitha_aragonda@epam.com>
2025-03-24 19:11:43 +0530

    Modified the files

--- src/test/java/corejava/Task04_HometaskString_01.java
+++ src/test/java/corejava/Task04_HometaskString_01.java
@@ -3,27 +3,27 @@ import java.util.HashSet;
 
 public class Task04_HometaskString_01 {
     public static void main(String[] args) {
-        String str = "programming";  // Example input
+        String str = "programming";  
         findDuplicates(str);
     }
 
     public static void findDuplicates(String str) {
-        HashSet<Character> seenChars = new HashSet<>();  // Set to store characters that we've seen
-        HashSet<Character> duplicates = new HashSet<>();  // Set to store duplicate characters
+        HashSet<Character> seenChars = new HashSet<>();  
+        HashSet<Character> duplicates = new HashSet<>();  
 
-        // Loop through each character in the string
+       
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
 
-            // If the character is already in 'seenChars', it's a duplicate
+          
             if (seenChars.contains(c)) {
-                duplicates.add(c);  // Add to duplicates set
+                duplicates.add(c);  
             } else {
-                seenChars.add(c);  // Otherwise, add it to 'seenChars'
+                seenChars.add(c);  
             }
         }
 
-        // Print out the duplicate characters
+      
         if (duplicates.isEmpty()) {
             System.out.println("No duplicates found.");
         } else {
--- src/test/java/corejava/Task04_HometaskString_02.java
+++ src/test/java/corejava/Task04_HometaskString_02.java
@@ -1,10 +1,35 @@
 package corejava;
 
+import java.util.HashSet;
+
 public class Task04_HometaskString_02 {
+    public static void main(String[] args) {
+        String str = "programming";  
+        findDuplicates(str);
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void findDuplicates(String str) {
+        HashSet<Character> seenChars = new HashSet<>();  
+        HashSet<Character> duplicates = new HashSet<>();  
 
-	}
+       
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
 
+                if (seenChars.contains(c)) {
+                duplicates.add(c);  
+            } else {
+                seenChars.add(c);  
+            }
+        }
+
+               if (duplicates.isEmpty()) {
+            System.out.println("No duplicates found.");
+        } else {
+            System.out.println("Duplicate characters are: ");
+            for (char c : duplicates) {
+                System.out.println(c);
+            }
+        }
+    }
 }
